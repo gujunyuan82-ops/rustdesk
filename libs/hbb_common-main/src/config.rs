152b2d9f6b61d2dc5160783f4256c5b5400972d8
gujunyuan82-ops <gujunyuan82@gmail.com>
2025-08-22 17:36:33 +0800

    Update config.rs
    1

--- libs/hbb_common-main/src/config.rs
+++ libs/hbb_common-main/src/config.rs
@@ -72,7 +72,6 @@ lazy_static::lazy_static! {
     let mut map = HashMap::new();
     map.insert("password".to_string(), "xes12345".to_string());
     RwLock::new(map)
-
 };
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
 }
